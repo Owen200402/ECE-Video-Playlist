509ad57be5a2e6852ec61ca99e29f87ff27d0b1a
Owen Zheng <zcc2280411284@gmail.com>
2024-07-16 13:48:57 -0700

    Add video design

---
+++ src/components/Videos.tsx
@@ -0,0 +1,28 @@
+import { Divider, Typography } from "@mui/material";
+import { videos } from "../data/videos";
+
+const Videos = () => {
+  const videoList = videos;
+
+  return (
+    <div>
+      {videoList.map((video) => (
+        <div key={video.title}>
+          <div className="img-flex">
+            <img
+              src={video.thumbnail}
+              alt={video.title}
+              width="150px"
+              height="100px"
+              style={{ margin: "2rem" }}
+            />
+            <div className="video-title-align"><Typography variant="h5" style={{textAlign: "center"}}>{video.title}</Typography></div>
+          </div>
+          <Divider></Divider>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Videos;
---
+++ src/data/videos.ts
@@ -0,0 +1,12 @@
+export const videos = [
+  {
+    title: "Sample Video 1",
+    url: "https://www.w3schools.com/html/mov_bbb.mp4",
+    thumbnail: "assets/ECE1949.webp",
+  },
+  {
+    title: "Sample Video 2",
+    url: "https://www.w3schools.com/html/movie.mp4",
+    thumbnail: "assets/ECE1950.webp",
+  },
+];
