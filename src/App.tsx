76f0e51a025e02d7f046bd537df9cc26cd4e7a73
Owen Zheng <zcc2280411284@gmail.com>
2024-07-16 14:51:23 -0700

    Add copyright

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,36 @@ function App() {
   return (
     <div className="big-wrapper zoom-container">
       <Grid container spacing={2}>
-        <Grid item xs={3}>
-        </Grid>
+        <Grid item xs={3}></Grid>
         <Grid item xs={6}>
           <div>
-            <Typography variant="h3" margin={4} style={{textAlign: "center", fontFamily: "American Typewriter, serif", color: "#002145"}}>
+            <Typography
+              variant="h3"
+              margin={4}
+              style={{
+                textAlign: "center",
+                fontFamily: "American Typewriter, serif",
+                color: "#002145",
+              }}
+            >
               ECE Faculty Playlist
             </Typography>
             <Videos></Videos>
+            <Typography
+              variant="body2"
+              margin={4}
+              style={{
+                textAlign: "center",
+                fontFamily: "cursive",
+                color: "#002145",
+              }}
+            >
+              &copy; {new Date().getFullYear()} UBC Electrical and Computer
+              Engineering. All rights reserved.
+            </Typography>
           </div>
         </Grid>
-        <Grid item xs={3}>
-        </Grid>
+        <Grid item xs={3}></Grid>
       </Grid>
     </div>
   );
