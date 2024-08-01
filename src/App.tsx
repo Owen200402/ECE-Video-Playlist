f87ec8773014813f0bf8be198b93206bbc6c201b
Owen Zheng <zcc2280411284@gmail.com>
2024-08-01 12:34:45 -0700

    Increase margin for the title

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,45 @@ import Videos from "./components/Videos";
 
 function App() {
   return (
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={2} lg={2} xl={2}></Grid>
-        <Grid item xs={12} sm={8} lg={8} xl={8}>
-          <div style={{display: "flex", flexDirection:"column", justifyContent:"center", alignItems:"center"}}>
-            <Typography
-              variant="h3"
-              margin={4}
-              style={{
-                textAlign: "center",
-                fontFamily: "American Typewriter, serif",
-                color: "#002145",
-              }}
-            >
-              ECE Faculty Playlist
-            </Typography>
-            <Videos />
-            <Typography
-              variant="body2"
-              margin={4}
-              style={{
-                textAlign: "center",
-                fontFamily: "cursive",
-                color: "#002145",
-              }}
-            >
-              &copy; {new Date().getFullYear()} UBC Electrical and Computer
-              Engineering. All rights reserved.
-            </Typography>
-          </div>
-        </Grid>
-        <Grid item xs={12} sm={2} lg={2} xl={2}></Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={2} lg={2} xl={2}></Grid>
+      <Grid item xs={12} sm={8} lg={8} xl={8}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            variant="h3"
+            margin={5}
+            style={{
+              textAlign: "center",
+              fontFamily: "American Typewriter, serif",
+              color: "#002145",
+            }}
+          >
+            ECE Faculty Playlist
+          </Typography>
+          <Videos />
+          <Typography
+            variant="body2"
+            margin={4}
+            style={{
+              textAlign: "center",
+              fontFamily: "cursive",
+              color: "#002145",
+            }}
+          >
+            &copy; {new Date().getFullYear()} UBC Electrical and Computer
+            Engineering. All rights reserved.
+          </Typography>
+        </div>
       </Grid>
+      <Grid item xs={12} sm={2} lg={2} xl={2}></Grid>
+    </Grid>
   );
 }
 
