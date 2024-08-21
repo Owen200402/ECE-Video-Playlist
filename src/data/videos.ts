5722806d48b897fef87c14e9fe53e2c338efcbca
Owen Zheng <zcc2280411284@gmail.com>
2024-08-21 11:10:23 -0700

    Multiply videos by 4 (testing)

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -14,6 +14,51 @@ export const videos = [
     url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Graduate%20Training%20Video.mp4",
     thumbnail: "assets/trainingTA.webp",
   },
+  {
+    title: "ECE Lab Onboarding Video for Teaching Assistants",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20TA%20Training.mp4",
+    thumbnail: "assets/onboarding.webp",
+  },
+  {
+    title: "Undegraduate Makerspace Orientation",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Makerspace%20Undergraduate%20Orientation%20Video.mp4",
+    thumbnail: "assets/makerspace.webp",
+  },
+  {
+    title: "ECE Graduate Training Video RT and Support Facilities",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Graduate%20Training%20Video.mp4",
+    thumbnail: "assets/trainingTA.webp",
+  },
+  {
+    title: "ECE Lab Onboarding Video for Teaching Assistants",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20TA%20Training.mp4",
+    thumbnail: "assets/onboarding.webp",
+  },
+  {
+    title: "Undegraduate Makerspace Orientation",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Makerspace%20Undergraduate%20Orientation%20Video.mp4",
+    thumbnail: "assets/makerspace.webp",
+  },
+  {
+    title: "ECE Graduate Training Video RT and Support Facilities",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Graduate%20Training%20Video.mp4",
+    thumbnail: "assets/trainingTA.webp",
+  },
+  {
+    title: "ECE Lab Onboarding Video for Teaching Assistants",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20TA%20Training.mp4",
+    thumbnail: "assets/onboarding.webp",
+  },
+  {
+    title: "Undegraduate Makerspace Orientation",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Makerspace%20Undergraduate%20Orientation%20Video.mp4",
+    thumbnail: "assets/makerspace.webp",
+  },
+  {
+    title: "ECE Graduate Training Video RT and Support Facilities",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Graduate%20Training%20Video.mp4",
+    thumbnail: "assets/trainingTA.webp",
+  },
   
   
 ];
