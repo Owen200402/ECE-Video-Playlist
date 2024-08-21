7e6ba26df09e0ea1a4fd1c9a2157e6111587737b
Owen Zheng <zcc2280411284@gmail.com>
2024-08-21 13:49:24 -0700

    Add three more videos

--- src/data/videos.ts
+++ src/data/videos.ts
@@ -5,7 +5,7 @@ export const videos = [
     thumbnail: "assets/onboarding.webp",
   },
   {
-    title: "Undergraduate Makerspace Orientation",
+    title: "ECE Undergraduate Makerspace Orientation",
     url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Makerspace%20Undergraduate%20Orientation%20Video.mp4",
     thumbnail: "assets/makerspace.webp",
   },
@@ -14,4 +14,19 @@ export const videos = [
     url: "https://people.ece.ubc.ca/~eng-services/Videos/UBC%20ECE%20-%20Graduate%20Training%20Video.mp4",
     thumbnail: "assets/trainingTA.webp",
   },
+  {
+    title: "ECE Lab Safety Tutorial",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/ECE%20Safety%20Lab%20Tutorial.mp4",
+    thumbnail: "assets/safetyTutorial.webp",
+  },
+  {
+    title: "ECE Introduction for Grad Students",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/Grad_Intro_v5.mp4",
+    thumbnail: "assets/IntroToGrad.webp",
+  },
+  {
+    title: "ECE Graduate Student Safety Orientation",
+    url: "https://people.ece.ubc.ca/~eng-services/Videos/Grad%20Safety_v6.mp4",
+    thumbnail: "assets/GradSafetyTraining.webp"
+  }
 ];
