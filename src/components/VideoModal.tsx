d5f4c08a31ac2b53d42d410d4dd0a8de5642dcf6
khoa <khoa19102005@gmail.com>
2024-09-03 02:28:08 -0700

    fix import cause failure

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -1,35 +1,10 @@
-// A child component of PhotoList
+import React from 'react';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+import CloseIcon from '@mui/icons-material/Close';
+import './VideoModal.css';
 
-import CloseIcon from "@mui/icons-material/Close";
-import styled from "styled-components";
-import ReactPlayer from "react-player";
-
-const Modal = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 10px;
-  z-index: 1000;
-  max-width: 90%;
-  max-height: 90%;
-  text-align: center;
-  display: flex;
-  flex-direction: column;
-`;
-
-// Overlay defines the background of the modal
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0, 0, 0, 0.9);
-  z-index: 999;
-`;
-
-interface Props {
+interface VideoModalProps {
   isOpen: boolean;
   onClose: () => void;
   url: string;
